998c1d6d20956ae6a7fbed3c4d35b232321e97fd
Lucia <feliulucia0@gmail.com>
2022-12-22 15:10:31 -0300

    finalizado angular

--- src/app/componentes/administrador/expadmin/expadmin.component.ts
+++ src/app/componentes/administrador/expadmin/expadmin.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-expadmin',
   templateUrl: './expadmin.component.html',
   styleUrls: ['./expadmin.component.css']
 })
-export class ExpadminComponent {
+export class ExpadminComponent implements OnInit{
+  experiencies: any;
+  nombresito : string = '';
+  fecha : any;
+  constructor(private datos: DatosService){ }
+
+  ngOnInit(): void {
+    this.datos.getDatos().subscribe(data => {
+      this.experiencies = data.experiencias;
+      this.nombresito = data.nombre;
+    })
+  }
 
 }
