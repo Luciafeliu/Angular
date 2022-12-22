cbd8ef1ec78ed85bb1b453cd51e37b4fa3b62fc5
Lucia <feliulucia0@gmail.com>
2022-12-22 15:02:38 -0300

    finalizando angular

--- src/app/componentes/administrador/proyectosadmin/proyectosadmin.component.ts
+++ src/app/componentes/administrador/proyectosadmin/proyectosadmin.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-proyectosadmin',
   templateUrl: './proyectosadmin.component.html',
   styleUrls: ['./proyectosadmin.component.css']
 })
-export class ProyectosadminComponent {
+export class ProyectosadminComponent implements OnInit{
+  proyectos: any;
+  imagen : any;
+  constructor(private datos: DatosService){ }
+
+  ngOnInit(): void {
+    this.datos.getDatos().subscribe(data => {
+      this.proyectos = data.proyectos;
+      this.imagen = data.imagen;
+    })
+  }
 
 }
