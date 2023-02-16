338c9b546ba78adbe58a1418ed4e3398e9f2e027
Lucia <feliulucia0@gmail.com>
2023-02-16 17:00:37 -0300

    terminando formulariosreactivos

--- src/app/modales/ed/ed.component.ts
+++ src/app/modales/ed/ed.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Educacion } from 'src/app/componentes/entidades/educacion';
+import { EducacionService } from 'src/app/servicios/educacion.service';
 
 @Component({
   selector: 'app-ed',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./ed.component.css']
 })
 export class EdComponent {
+  educacionForm: FormGroup;
+  educacion: Educacion []=[];
+
+  constructor(private educacionService: EducacionService, private formBuilder: FormBuilder, private sExperiencia: ExperienciaService){
+    this.experienciaForm = this.formBuilder.group({
+      area : ['', [Validators.required]],
+      descripcion : ['', [Validators.required]],
+      fin : ['', [Validators.required]],
+      inicio : ['', [Validators.required]],
+      titulo : ['', [Validators.required]],
+    })
+  }
 
 }
